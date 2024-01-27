4a543b928079e435bda8a80386f588a5a00ae43e
drake13371337 <michalprago@gmail.com>
2024-01-28 00:49:03 +0300

    task templates created

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*bool valid = int.TryParse(Console.ReadLine(), out int actionCount);
+            for (int i = 0; i < actionCount; i++)
+            {
+
+            }*/
+        }
+    }
+}
